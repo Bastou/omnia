bba42496e02c0fe46bba88880be6c1ec506d5b32
Unknown <robin.minervini@gmail.com>
2018-03-22 13:36:52 +0100

    Popup target image detected

---
+++ Assets/SmoothPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothPosition : MonoBehaviour {
+
+	public Vector3 destination;
+	public float speed = 0.1f;
+
+	void Start () {
+		Debug.Log ("test");
+		destination = transform.position;
+	}
+
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, destination, speed * Time.deltaTime);
+	}
+}
