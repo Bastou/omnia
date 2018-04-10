2d96f69193480355957246831f8ec7f86a4561bf
Unknown <robin.minervini@gmail.com>
2018-04-10 15:03:46 +0200

    gitflow-feature-stash: unlock_content
    Unlock map scene on scan (on winston for the moment)

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl control;
+	public bool isMapUnlocked;
+
+	// Create a singleton GameControl to manage data persistence
+	void Awake () {
+		if (control == null) {
+			DontDestroyOnLoad (gameObject);
+			control = this;
+		} else if (control != this){
+			Destroy (gameObject);
+		}
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 100, 30), "isMapUnlocked: " + isMapUnlocked);
+	}
+
+	public void Save() {
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.persistentDataPath + "/data.dat");
+
+		PlayerData data = new PlayerData ();
+		data.isMapUnlocked = isMapUnlocked;
+
+		bf.Serialize (file, data);
+		file.Close();
+	}
+
+	public void Load() {
+		if (File.Exists (Application.persistentDataPath + "/data.dat")) {
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.persistentDataPath + "/data.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file); //Cast to generic object to PlayerData specifically
+			file.Close();
+
+			isMapUnlocked = data.isMapUnlocked;
+		}
+	}
+}
+
+// Tell to Unity we can save the data to a file
+[Serializable]
+class PlayerData {
+	public bool isMapUnlocked;
+}
