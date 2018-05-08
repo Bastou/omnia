3e1f0906b93ad9fe223890f38fcb1c01c9a8be22
Minervini Robin <robin.minervini@gmail.com>
2018-05-08 16:27:39 +0200

    Welcome, contract, start reading scene

--- Assets/Scripts/MenuCtrl.cs
+++ Assets/Scripts/MenuCtrl.cs
@@ -2,26 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuCtrl : MonoBehaviour {
 
-	UnityEngine.UI.Button button;
+	private Fading fading;
+	private Slider slider;
 
-	void Start() {
+	void Start()
+	{
+		// Setup fading
+		fading = GetComponent<Fading> ();
+		slider = GameObject.Find("Slider").GetComponent<Slider>();
+		print(slider);
 
-		// # Find btn map and set active if map is unlocked
-//		button = GameObject.Find("btnMap").GetComponent<UnityEngine.UI.Button>();
-//		Debug.Log ("isMapUnlocked = " + GameControl.control.isMapUnlocked);
-//		if (GameControl.control.isMapUnlocked == true) {
-//			button.gameObject.SetActive (true);
-//		} else {
-//			button.gameObject.SetActive (false);
-//		}
 	}
 
 	// SceneLoader
-	public void LoadScene(string sceneName)
-	{
-		SceneManager.LoadScene (sceneName);
-	}
+ 	public void LoadScene(string sceneName) {
+     		StartCoroutine(StartScene(sceneName));
+     }
+
+	IEnumerator StartScene(string sceneName) {
+		//button.onClick.AddListener(LoadScene()); -> load scene
+		float fadeTime = fading.BeginFade (1); // 1 cause fade out
+		yield return new WaitForSeconds(fadeTime);
+		//SceneManager.LoadScene (sceneName);	 	
+		AsyncOperation operation = SceneManager.LoadSceneAsync (sceneName);
+
+		//loadingScreen.SetActive (true);
+		//Debug.Log(slider);
+		if (slider) {
+			while (!operation.isDone) {
+				float progress = Mathf.Clamp01 (operation.progress / 0.9f);
+				//Debug.Log (progress);
+				slider.value = progress;
+				yield return null;
+			}
+		}
+ 	}	 	
+
 }
